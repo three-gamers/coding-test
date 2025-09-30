e4db59a2b319eefd09e7bbd1779b85b6b27b3280
keykat <koin12000@naver.com>
2025-09-30 11:58:28 +0900

    feat: 문제 풀이 수정

--- baekjoon/20057/keykat_20057.py
+++ baekjoon/20057/keykat_20057.py
@@ -1,17 +1,20 @@
-def moveDirection(curr, dir, map, result):
+def moveDirection(curr, nextCurr, dir, map):
+    result = 0
     currSand = map[curr[1]][curr[0]]
 
     for d in dir:
         newDir = (curr[0] + d[0], curr[1] + d[1])
         # 빠져나간 모래 양
         if newDir[0] >= len(map) or newDir[0] < 0 or newDir[1] >= len(map) or newDir[1] < 0:
-            result += (currSand * d[2] * 100) // 100
+            result += int(currSand * d[2])
+            map[curr[1]][curr[0]] -= int(currSand * d[2])
         else:
-            map[newDir[1]][newDir[0]] += (currSand * d[2] * 100) // 100
-    
+            map[newDir[1]][newDir[0]] += int(currSand * d[2])
+            map[curr[1]][curr[0]] -= int(currSand * d[2])
+        
+    map[nextCurr[1]][nextCurr[0]] += map[curr[1]][curr[0]]
     map[curr[1]][curr[0]] = 0
-
-    print(map)
+    return result
         
 
 N = int(input())
@@ -20,10 +23,10 @@ result = 0
 
 curr = (N // 2, N // 2)
 
-l = [(-1, 0, 0.55), (0, -2, 0.02), (0, -1, 0.07), (0, 1, 0.07), (0, 2, 0.02), (1, -1, 0.01), (1, 1, 0.01), (-1, -1, 1), (-1, 1, 1), (-2, 0, 0.05)]
-r = [(1, 0, 0.55), (0, -2, 0.02), (0, -1, 0.07), (0, 1, 0.07), (0, 2, 0.02), (-1, -1, 0.01), (-1, 1, 0.01), (1, -1, 1), (1, 1, 1), (2, 0, 0.05)]
-u = [(0, -1, 0.55), (-2, -1, 0.02), (-1, -1, 0.07), (1, -1, 0.07), (2, -1, 0.02), (-1, 0, 0.01), (1, 0, 0.01), (-1, -2, 1), (1, -2, 1), (0, -2, 0.05)]
-d = [(0, 1, 0.55), (-2, 1, 0.02), (-1, 1, 0.07), (1, 1, 0.07), (2, 1, 0.02), (-1, 0, 0.01), (1, 0, 0.01), (-1, 2, 1), (1, 2, 1), (0, 2, 0.05)]
+l = [(-1, 0, 0), (0, -2, 0.02), (0, -1, 0.07), (0, 1, 0.07), (0, 2, 0.02), (1, -1, 0.01), (1, 1, 0.01), (-1, -1, 0.1), (-1, 1, 0.1), (-2, 0, 0.05)]
+r = [(1, 0, 0), (0, -2, 0.02), (0, -1, 0.07), (0, 1, 0.07), (0, 2, 0.02), (-1, -1, 0.01), (-1, 1, 0.01), (1, -1, 0.1), (1, 1, 0.1), (2, 0, 0.05)]
+u = [(0, -1, 0), (-2, -1, 0.02), (-1, -1, 0.07), (1, -1, 0.07), (2, -1, 0.02), (-1, 0, 0.01), (1, 0, 0.01), (-1, -2, 0.1), (1, -2, 0.1), (0, -2, 0.05)]
+d = [(0, 1, 0), (-2, 1, 0.02), (-1, 1, 0.07), (1, 1, 0.07), (2, 1, 0.02), (-1, 0, 0.01), (1, 0, 0.01), (-1, 2, 0.1), (1, 2, 0.1), (0, 2, 0.05)]
 
 # left 1번, down 1번
 # right 2번, up 2번
@@ -35,22 +38,68 @@ d = [(0, 1, 0.55), (-2, 1, 0.02), (-1, 1, 0.07), (1, 1, 0.07), (2, 1, 0.02), (-1
 # 이 때 curr 좌표 + l 배열의 좌표가 밖으로 튀어나가면 연산
 
 for i in range(N):
-    for ii in range(i):
         # right, up 방향
-        if i % 2 == 0:
-            curr = (curr[0], curr[1] + 1)
-            moveDirection(curr, r, map, result)
+        if i % 2 != 0:
+            for ii in range(i + 1):
+                nextCurr = (curr[0] + 1, curr[1])
+                print("right")
+                result += moveDirection(curr, nextCurr, r, map)
+                curr = nextCurr
 
-            curr = (curr[0] - 1, curr[1])
-            moveDirection(curr, d, map, result)
+                for y in map:
+                    print(y)
+                print("\n")
+
+                if curr[0] < 0 or curr[0] >= len(map) or curr[1] < 0 or curr[1] >= len(map):
+                    break
+
+            for ii in range(i + 1):
+                nextCurr = (curr[0], curr[1] - 1)
+                print("up")
+                result += moveDirection(curr, nextCurr, u, map)
+                curr = nextCurr
+
+                for y in map:
+                    print(y)
+                print("\n")
+
+                if curr[0] < 0 or curr[0] >= len(map) or curr[1] < 0 or curr[1] >= len(map):
+                    break
+
+
         # left, down 방향
         else: 
-            curr = (curr[0], curr[1] - 1)
-            moveDirection(curr, r, map, result)
+            for ii in range(i + 1):
+                nextCurr = (curr[0] - 1, curr[1])
+                print("left")
+                result += moveDirection(curr, nextCurr, l, map)
+                curr = nextCurr
 
-            curr = (curr[0] + 1, curr[1])
-            moveDirection(curr, d, map, result)
 
-print(result)
+                for y in map:
+                    print(y)
+                print("\n")
+
+                if curr[0] < 0 or curr[0] >= len(map) or curr[1] < 0 or curr[1] >= len(map):
+                    break
+
+            for ii in range(i + 1):
+                nextCurr = (curr[0], curr[1] + 1)
+                print("down")
+                result += moveDirection(curr, nextCurr, d, map)
+                curr = nextCurr
+
+
+                for y in map:
+                    print(y)
+                print("\n")
+
+                if curr[0] < 0 or curr[0] >= len(map) or curr[1] < 0 or curr[1] >= len(map):
+                    break
+
+        if curr[0] < 0 or curr[0] >= len(map) or curr[1] < 0 or curr[1] >= len(map):
+            break
+
+print(int(result))
 
 
