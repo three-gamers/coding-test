f49e8d79c510bf27262e4433947c1450c5f30684
keykat <koin12000@naver.com>
2025-10-27 11:35:23 +0900

    feat: chatGPT 정답

--- programmers/340210/keykat_340210.py
+++ programmers/340210/keykat_340210.py
@@ -1,137 +1,105 @@
 def calc(a, b, exp, n):
-    aStr = str(a)
-    bStr = str(b)
-
-    li = [str(i) for i in range(n, 10)]
-    if any(n in aStr for n in li) or any(n in bStr for n in li):
+    """진법 n에서 a와 b의 연산 결과를 반환"""
+    # 진법 n 이상의 숫자가 포함되어 있으면 불가능
+    a_str = str(a)
+    b_str = str(b)
+    
+    for digit in a_str:
+        if int(digit) >= n:
+            return -1
+    for digit in b_str:
+        if int(digit) >= n:
+            return -1
+    
+    # 10진법으로 변환
+    a_decimal = 0
+    b_decimal = 0
+    
+    for i, digit in enumerate(reversed(a_str)):
+        a_decimal += int(digit) * (n ** i)
+    
+    for i, digit in enumerate(reversed(b_str)):
+        b_decimal += int(digit) * (n ** i)
+    
+    # 연산 수행
+    if exp == '+':
+        result_decimal = a_decimal + b_decimal
+    elif exp == '-':
+        result_decimal = a_decimal - b_decimal
+        if result_decimal < 0:
+            return -1
+    else:
         return -1
-
-    cStr = bStr if len(aStr) > len(bStr) else aStr
-    result = '' 
-
-    listA = list(aStr)
-    listB = list(bStr)
-
-    if exp == '-':
-        for idx, _ in enumerate(cStr):
-            aCurrIndex = len(aStr) - idx - 1
-            bCurrIndex = len(bStr) - idx - 1
-
-            if int(listA[aCurrIndex]) < int(listB[bCurrIndex]):
-                tempIndex = aCurrIndex - 1
-                while int(listA[tempIndex]) <= 0:
-                    listA[tempIndex] = n - 1
-                    tempIndex = tempIndex - 1
-                
-
-                listA[tempIndex] = str(int(listA[tempIndex]) - 1)
-                listA[aCurrIndex] = str(n - int(listB[bCurrIndex]) + int(listA[aCurrIndex]))
-            
-            else:
-                listA[aCurrIndex] = str(int(listA[aCurrIndex]) - int(listB[bCurrIndex]))
-
-    elif exp == '+':
-        for idx, _ in enumerate(cStr):
-            aCurrIndex = len(aStr) - idx - 1
-            bCurrIndex = len(bStr) - idx - 1
-
-            if int(listA[aCurrIndex]) + int(listB[bCurrIndex]) >= n:
-                tempIndex = aCurrIndex - 1
-                if tempIndex != -1:
-                    listA[tempIndex] = str(int(listA[tempIndex]) + 1)
-
-                    while int(listA[tempIndex]) == n - 1:
-                        listA[tempIndex] = str(int(listA[tempIndex]) + 1)
-                        tempIndex = tempIndex - 1
-
-                        if tempIndex == -1:
-                            break
-
-                listA[aCurrIndex] = str(int(listB[bCurrIndex]) + int(listA[aCurrIndex]) - n)
-                if tempIndex == -1:
-                    listA.insert(0, '1')
-
-            else:
-                listA[aCurrIndex] = str(int(listB[bCurrIndex]) + int(listA[aCurrIndex]))
-
-    return int(''.join(listA))
-
-
+    
+    # 결과를 진법 n으로 변환
+    if result_decimal == 0:
+        return 0
+    
+    result_n = ""
+    while result_decimal > 0:
+        result_n = str(result_decimal % n) + result_n
+        result_decimal //= n
+    
+    return int(result_n)
 
 
 def solution(expressions):
     answer = []
-
-    maxFormation = 0
-    for exp in expressions:
-        expList = exp.split(' ')
-        a = expList[0]
-        b = expList[2]
-
-        # a와 b의 각 자릿수를 int로 변환하여 최대값 찾기
-        maxFormation = max(maxFormation, max(map(int, a)) + 1)
-        maxFormation = max(maxFormation, max(map(int, b)) + 1)
-
-    possibleFormations = list(range(maxFormation, 10))
-    for exp in expressions:
-        expList = exp.split(' ')
-        a = expList[0]
-        e = expList[1]
-        b = expList[2]
-        res = expList[4]
-        formations = set()
-
-        if res != 'X':
-            for i in range(maxFormation, 10):
-                if int(res) == calc(int(a), int(b), e, i):
-                    # print('i::::::', i)
-                    formations.add(i)
-
-                # print(int(res), calc(int(a), int(b), e, i))
-
-            if len(formations) == 1:
-                # print("sdfdasfdas: ", formations)
-                possibleFormations = [formations.pop()]
-                break
-
-            # print("formations::: ", formations)
     
-            for pf in possibleFormations:
-                if pf not in formations:
-                    possibleFormations.remove(pf)
-
-    # print(possibleFormations)
-
+    # 최소 진법 찾기
+    min_base = 2
     for exp in expressions:
-        expList = exp.split(' ')
-        a = expList[0]
-        e = expList[1]
-        b = expList[2]
-        equal = expList[3]
-        res = expList[4]
-        results = set()
-
+        exp_list = exp.split(' ')
+        a = exp_list[0]
+        b = exp_list[2]
+        
+        for digit in a + b:
+            min_base = max(min_base, int(digit) + 1)
+    
+    # 가능한 진법들 찾기
+    possible_bases = set(range(min_base, 10))
+    
+    # 알려진 식들로 가능한 진법들을 좁혀나가기
+    for exp in expressions:
+        exp_list = exp.split(' ')
+        a = exp_list[0]
+        e = exp_list[1]
+        b = exp_list[2]
+        res = exp_list[4]
+        
+        if res != 'X':
+            valid_bases = set()
+            for base in range(min_base, 10):
+                if calc(int(a), int(b), e, base) == int(res):
+                    valid_bases.add(base)
+            
+            # 교집합으로 가능한 진법 업데이트
+            possible_bases = possible_bases.intersection(valid_bases)
+            
+            # 가능한 진법이 없으면 빈 배열 반환
+            if not possible_bases:
+                return []
+    
+    # X가 있는 식들의 결과 계산
+    for exp in expressions:
+        exp_list = exp.split(' ')
+        a = exp_list[0]
+        e = exp_list[1]
+        b = exp_list[2]
+        equal = exp_list[3]
+        res = exp_list[4]
+        
         if res == 'X':
-            for form in possibleFormations:
-                results.add(calc(int(a), int(b), e, form))
-
-        else:
-            continue
-
-        if len(results) == 0 or len(results) > 1:
-            answer.append(a + ' ' + e + ' ' + b + ' ' + equal + ' ' + '?')
-        else:
-            answer.append(a + ' ' + e + ' ' + b + ' ' + equal + ' ' + str(results.pop()))
-
-
+            results = set()
+            for base in possible_bases:
+                result = calc(int(a), int(b), e, base)
+                if result != -1:
+                    results.add(result)
+            
+            if len(results) == 1:
+                answer.append(f"{a} {e} {b} {equal} {list(results)[0]}")
+            else:
+                answer.append(f"{a} {e} {b} {equal} ?")
+    
     return answer
 
-
-
-
-
-# print(solution(["14 + 3 = 17", "13 - 6 = X", "51 - 5 = 44"]))
-# print(solution(["1 + 1 = 2", "1 + 3 = 4", "1 + 5 = X", "1 + 2 = X"]))
-# print(solution(["10 - 2 = X", "30 + 31 = 101", "3 + 3 = X", "33 + 33 = X"]))
-# print(solution(["2 - 1 = 1", "2 + 2 = X", "7 + 4 = X", "5 - 5 = X"]))
-# print(solution(["2 - 1 = 1", "2 + 2 = X", "7 + 4 = X", "8 + 4 = X"]))
